e0d10b636850502d89dc158e52df294971fc5067
MF_PC <MF_PC@10.227.78.22>
2018-07-27 13:28:43 +0800

    According to IdCards. the big difference between original card and fake cards is that the original card has two faces while other cards has one face. therefore a face detection can be used to recognize faces and count the number of faces in each image to find the original and fake image.Accordingly, a face detection classifier called casecadeClassifier from OpenCv is used to find faces inside each image.

--- IdCardIdentification/src/Main.java
+++ IdCardIdentification/src/Main.java
@@ -1,6 +1,8 @@
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfRect;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.objdetect.CascadeClassifier;
 
 public class Main {
 
@@ -12,7 +14,17 @@ public class Main {
 		// The following codes read an IdCard from image and saving it in a Matrix 
 	      String file ="C:\\Users\\MF_PC\\git\\repository5\\IdCardIdentification\\src\\idcards\\1.jpg";
 	      Mat src = Imgcodecs.imread(file);
-	      System.out.print("Image loaded .......");  // write in Console if image loaded
+	      System.out.println("Image loaded");  // write "Image Loaded ..." in Console if image loaded
+	      
+	      // Instantiating the CascadeClassifier for Face Detection
+	      String xmlClassifier = "C:\\Users\\MF_PC\\eclipse-workspace\\OCV\\src\\fxml\\lbpcascade_frontalface.xml";
+	      CascadeClassifier classifier = new CascadeClassifier(xmlClassifier);
+	      
+	   // Detecting the face in the snap
+	      MatOfRect faceDetections = new MatOfRect();
+	      classifier.detectMultiScale(src, faceDetections);
+	      System.out.println(String.format("Number of Detected faces: %s",faceDetections.toArray().length));
+
 	      
 	}
 
