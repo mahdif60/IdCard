09308b5df1878a94d7d0c431f922c1e1a9703ab0
MF_PC <MF_PC@10.227.78.22>
2018-07-27 12:38:49 +0800

    Adding Images and call an image by OPenCV library. Then store image to a matrix .

--- IdCardIdentification/src/Main.java
+++ IdCardIdentification/src/Main.java
@@ -1,9 +1,19 @@
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );  // Loading OpenCV core library
+	      
+		// The following codes read an IdCard from image and saving it in a Matrix 
+	      String file ="C:\\Users\\MF_PC\\git\\repository5\\IdCardIdentification\\src\\idcards\\1.jpg";
+	      Mat src = Imgcodecs.imread(file);
+	      System.out.print("Image loaded .......");  // write in Console if image loaded
+	      
 	}
 
 }
