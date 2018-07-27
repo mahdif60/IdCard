70b16a2fff917d4d925a6153bc377b9053b27100
MF_PC <MF_PC@10.227.78.22>
2018-07-27 13:47:47 +0800

    codes modified to accept all images and return the result as: 1. image number 2. Number of detected face 3. Originality

--- IdCardIdentification/src/Main.java
+++ IdCardIdentification/src/Main.java
@@ -12,10 +12,11 @@ public class Main {
 		System.loadLibrary( Core.NATIVE_LIBRARY_NAME );  // Loading OpenCV core library
 	      
 		// The following codes read an IdCard from image and saving it in a Matrix 
-	      String file ="C:\\Users\\MF_PC\\git\\repository5\\IdCardIdentification\\src\\idcards\\1.jpg";
+		int j=1;
+		for(j=1;j<5;j++){
+	      String file ="C:\\Users\\MF_PC\\git\\repository5\\IdCardIdentification\\src\\idcards\\"+j+".jpg";
 	      Mat src = Imgcodecs.imread(file);
-	      System.out.println("Image loaded");  // write "Image Loaded ..." in Console if image loaded
-	      
+	      System.out.println("Image Number:"+j);  // write "Image Loaded ..." in Console if image loaded
 	      // Instantiating the CascadeClassifier for Face Detection
 	      String xmlClassifier = "C:\\Users\\MF_PC\\eclipse-workspace\\OCV\\src\\fxml\\lbpcascade_frontalface.xml";
 	      CascadeClassifier classifier = new CascadeClassifier(xmlClassifier);
@@ -23,9 +24,17 @@ public class Main {
 	   // Detecting the face in the snap
 	      MatOfRect faceDetections = new MatOfRect();
 	      classifier.detectMultiScale(src, faceDetections);
-	      System.out.println(String.format("Number of Detected faces: %s",faceDetections.toArray().length));
-
+	      int i=faceDetections.toArray().length;
+	      System.out.println(String.format("Number of Detected faces: %s",i));
+          
+	      if (i<2) {
+	    	  System.out.println("This is a Fake IdCard");
+	    	  System.out.println("***************************");
+	      }else {
+	    	  System.out.println("This is an Original IdCard");
+	    	  System.out.println("***************************");
+	      }
 	      
 	}
-
+	}
 }
